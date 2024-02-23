645f645c61e02c01889e4afecf571cde07ebdebc
FedericaSidoti <federicasidoti21@gmail.com>
2024-02-23 18:43:09 +0100

    FIX- graphic and data

--- src/fakedata.tsx
+++ src/fakedata.tsx
@@ -7,17 +7,17 @@ export const fakeData = [
             {
             date: '10/01/2020 15:30:55',
             message: 'Hai portato a spasso il drago?',
-            status: 'sent'
+            status: 'received'
             },
             {
             date: '10/01/2020 15:50:00',
             message: 'Ricordati di portarmi il vino',
-            status: 'sent'
+            status: 'received'
             },
             {
             date: '10/01/2020 16:15:22',
             message: 'Tutto fatto!',
-            status: 'received'
+            status: 'sent'
             }
         ],
     },
@@ -29,17 +29,17 @@ export const fakeData = [
             {
             date: '20/03/2020 16:30:00',
             message: 'Ciao come sta Daenerys?',
-            status: 'sent'
+            status: 'received'
             },
             {
             date: '20/03/2020 16:30:55',
             message: 'Bene grazie! Stasera ci vediamo?',
-            status: 'received'
+            status: 'sent'
             },
             {
             date: '20/03/2020 16:35:00',
             message: "Mi piacerebbe ma l'Inverno sta Arrivando.",
-            status: 'sent'
+            status: 'received'
             }
         ],
     },
